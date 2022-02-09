67b760592bee695a700a923af7f8439f33aef471
Denilson-Santuchi <denilsonsantuchi@hotmail.com>
2022-02-09 17:58:20 -0300

    first requirement

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,89 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loginAction } from '../actions/index';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      isButtonOk: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onClickButton = this.onClickButton.bind(this);
+  }
+
+  onClickButton(e) {
+    e.preventDefault();
+    const { email, password } = this.state;
+    const { saveUser, history } = this.props;
+    saveUser({ email, password });
+    history.push('/carteira');
+  }
+
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    }, () => {
+      const VALIDATION = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      // https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript
+      const SIX = 6;
+      this.setState(({ password, email }) => ({
+        isButtonOk: VALIDATION.test(email) && password.length >= SIX,
+      }));
+    });
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isButtonOk } = this.state;
+    return (
+      <section>
+        <h1>Login</h1>
+        <form>
+          <label htmlFor="email-input">
+            Email:
+            <input
+              type="email"
+              id="email-input"
+              value={ email }
+              name="email"
+              data-testid="email-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password-input">
+            Senha:
+            <input
+              type="password"
+              id="password-input"
+              value={ password }
+              name="password"
+              data-testid="password-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={ !isButtonOk }
+            onClick={ this.onClickButton }
+          >
+            Entrar
+          </button>
+        </form>
+      </section>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  saveUser: PropTypes.func,
+  push: PropTypes.func,
+}.isRequired;
+
+const mapDispatchToProps = (dispatch) => ({
+  saveUser: (payload) => dispatch(loginAction(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,23 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+
+import { USER_LOGIN } from '../actions/index';
+
+const INTIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+const userReducer = (state = INTIAL_STATE, action) => {
+  switch (action.type) {
+  case USER_LOGIN:
+    return {
+      ...state,
+      email: action.payload.email,
+      password: action.payload.password,
+    };
+  default:
+    return state;
+  }
+};
+
+export default userReducer;
